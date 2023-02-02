7bb5f4f74211806c8856234dc5d0d8d316b21898
esleyngb <esleyngb@BP-5LD57T3-INEA.domain.rj.gov.br>
2023-02-02 17:20:50 -0300

    Arquitetura Hexagonal com Nest.js | Parte 1

---
+++ src/lists/lists.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateListDto } from './dto/create-list.dto';
+import { UpdateListDto } from './dto/update-list.dto';
+import { List } from './entities/list.entity';
+import { InjectModel } from '@nestjs/sequelize';
+
+@Injectable()
+export class ListsService {
+  constructor(
+    @InjectModel(List)
+    private listModel: typeof List,    
+  ){}
+
+  create(createListDto: CreateListDto) {
+    return this.listModel.create(createListDto);
+  }
+
+  findAll() {
+    return this.listModel.findAll();
+  }
+
+  async findOne(id: number) {
+    const list = await this.listModel.findByPk(id);
+    if (!list){
+      throw new Error('List not Found');
+    }
+    return List;
+  }
+
+  update(id: number, updateListDto: UpdateListDto) {
+    return `This action updates a #${id} list`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} list`;
+  }
+}
